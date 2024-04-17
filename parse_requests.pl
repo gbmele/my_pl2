0beecf6428008653176d88884d66d4a2c851579b
gbmele <90426799+gbmele@users.noreply.github.com>
2024-04-17 09:33:24 +0000

    ww
    dd

---
+++ parse_requests.pl
@@ -0,0 +1,35 @@
+request(["qq",o,a,"A","a","am","MH0900-1800(9:00)","MH0800-1800(10:00)","MH0900-1730(9:50)", "p","pm","al","AL","cl","CL","lsl","LSL","LPPA","CONF","PAT","mat","ua","UA","up","UP","c","cs","CS"]).
+
+get_sub(SH,a):-
+  sub_string("a am A AM MH0900-1800(9:00) MH0800-1800(10:00) MH0900-1730(9:50)",_,_,_,SH).
+get_sub(SH,p):-
+ sub_string("p P PM pm MH1600-2400(8.00) MH1400-2400(8.00) MH1700-2400(8.00) 14:00-23:59",_,_,_,SH).
+get_sub(SH,al):-
+  sub_string("al AL AL(0:00) AL (0:00) AL(10:00)",_,_,_,SH).
+get_sub(SH,c):-
+sub_string("c cs C CS",_,_,_,SH).
+get_sub(SH,cl):-
+  sub_string("cl CL CONF CONF(0:00) CONF (0:00) CONF(10:00) CONF (10:00)",_,_,_,SH).
+get_sub(SH,lsl):-
+  sub_string("lsl LSL LPPA ",_,_,_,SH).
+get_sub(SH,mat):-
+  sub_string("mat MAT",_,_,_,SH).
+get_sub(SH,pat):-
+  sub_string("pat PAT",_,_,_,SH).
+get_sub(SH,ua):-
+  sub_string("uaUA",_,_,_,SH).
+get_sub(SH,up):-
+  sub_string("upUP",_,_,_,SH).
+get_sub(SH,u):-
+  sub_string("u U U00:23:59 U 00-23:59",_,_,_,SH).
+get_sub(SH,o):-
+  sub_string("oOoffOFF",_,_,_,SH).
+get_sub(SH,x):-
+  sub_string("xss",_,_,_,SH).
+
+get_sub(_,"EEEEEERRRRRRROOOOOOOOORRRRRRR").
+
+%%ts=test_substrings  
+ts(R,RR):- 
+ request(R),
+ maplist(get_sub,R,RR).
